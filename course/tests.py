dca1335b68d735d665eb3c635ef09d3e323ee121
Maggie Hibberd <maggiehibberd@gmail.com>
2023-04-04 13:37:36 +0100

    Working on test on another laptop, not complete but pushing to git for access

--- course/tests.py
+++ course/tests.py
@@ -1,3 +1,22 @@
-from django.test import TestCase
+import unittest
 
-# Create your tests here.
+from django.test import TestCase
+from .models import Course
+
+
+class CourseTestCase(TestCase):
+    def setUp(self):
+        Course.objects.create(name="Computer Science", description="It's programming and shizzle innit")
+        Course.objects.create(name="City & Guilds - Painting and Decorating", description="Get on the painting and "
+                                                                                          "decorating flex!")
+
+    def test_get_my_course(self):
+        """Animals that can speak are correctly identified"""
+        cs = Course.objects.get(name="Computer Science")
+        pd = Course.objects.get(name="City & Guilds - Painting and Decorating")
+        self.assertEqual(cs.name(), "Computer Science")
+        self.assertEqual(pd.name(), "City & Guilds - Painting and Decorating")
+
+
+if __name__ == '__main__':
+    unittest.main()
